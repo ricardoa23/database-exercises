ffd3bb83be6eb19d0e64615641f07e7c67395ace
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2023-03-03 15:06:37 -0600

    completed mysql tables exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,10 +1,12 @@
 USE codeup_test_db;
 
+drop table if exists albums;
+
 CREATE TABLE albums (
     id int unsigned not null auto_increment,
     artist varchar(100) not null,
     name varchar(100) not null,
-    release_date date,
+    release_date smallint unsigned,
     sales decimal(5, 2),
     genre varchar(30),
     primary key (id)
